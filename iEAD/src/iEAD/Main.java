849460d6f1a7dcb56905f5d2c6d18b1c6f0daf4f
Rodrigo Lima de Souto Leandro <elementar@gmail.com>
2015-11-24 12:27:10 -0300

    Melhoria estetica do codigo e termino da função Amigo.

--- iEAD/src/iEAD/Main.java
+++ iEAD/src/iEAD/Main.java
@@ -26,20 +26,19 @@ public class Main {
         contas = new ArrayList<Conta>();
         amigos = new ArrayList<Amigo>();
   
-        System.out.println("Bem Vindo ao iEAD");  
+        System.out.println("Bem Vindo ao iEAD \n");  
   
         while (true) {  
             String opcao = menuCadastro();  
   
-            if (opcao.equalsIgnoreCase("c")) {  
+            if (opcao.equalsIgnoreCase("c"))  
                 cadastrar();  
-            } else if (opcao.equalsIgnoreCase("l")) {  
+            
+            if (opcao.equalsIgnoreCase("l"))  
                 login();
-            } else if (opcao.equalsIgnoreCase("x")) {
+            
+            if (opcao.equalsIgnoreCase("x"))
             	break;
-            } else {  
-                System.out.println("\nOpção Inválida \n");  
-            }  
         }  
 
 	}
@@ -48,42 +47,47 @@ public class Main {
 		  
         while (true) {  
         	
-            System.out.println("Cadastro de Usuário");  
+            System.out.println("\nCadastro de Usuário");  
             Conta usuario = new Conta();  
             usuario.setLogin(entradaTexto("Login:"));
             if (retornaLogin(usuario.getLogin()) == true) {
-            	System.out.println("Usuario já cadastrado, reiniciando cadastro.");
+            	System.out.println("\nUsuario já cadastrado, reiniciando cadastro. \n");
             	continue;
             }
+            
             usuario.setSenha(entradaTexto("Senha: "));  
             usuario.setNome(entradaTexto("Nome: "));
             usuario.setCpf(entradaNumero("CPF: "));
             if (retornaLogin(usuario.getCpf()) == true) {
-            	System.out.println("CPF já cadastrado, reiniciando cadastro.");
+            	System.out.println("\nCPF já cadastrado, reiniciando cadastro.\n");
             	continue;
             }
+            
             usuario.setEndereco(entradaTexto("Endereço: "));
             usuario.setTelefone(entradaNumero("Telefone: "));
   
-            String cadastrar = entradaTexto("Adicionar usuário (S/N) ?");  
+            String cadastrar = entradaTexto("\nAdicionar usuário (S/N) ?");  
             if (cadastrar.equalsIgnoreCase("s")) {  
-                System.out.println("Cadastro adicionado !!!");  
+                System.out.println("\nCadastro adicionado !!! \n");  
                 contas.add(usuario);
                 break;
-            } else if (cadastrar.equalsIgnoreCase("n")){  
-                System.out.println("Cadastro ignorado !!!");
+            }
+            
+            if (cadastrar.equalsIgnoreCase("n")){  
+                System.out.println("\nCadastro ignorado !!! \n");
                 
-                String continua = entradaTexto("Continuar cadastrando (S/N) ?");  
-                if (continua.equalsIgnoreCase("N")) {  
+                String continua = entradaTexto("\nContinuar cadastrando (S/N) ?");  
+                if (continua.equalsIgnoreCase("N"))  
                     break;  
-                } else if (continua.equalsIgnoreCase("s")){  
-                    // se for s sai do if e volta para o inicio do while  
-                } else {  
+                
+                if (continua.equalsIgnoreCase("s"))
+                	continue;
+                else {  
                     System.out.println("\nOpção inválida \n");  
                     break;  
                 }
-            } else {  
-                System.out.println("\nOpção inválida \n");
+            }
+            else {
                 break;
             }  
     
@@ -94,43 +98,44 @@ public class Main {
 		System.out.println("\nTela de Login \n");
 		String user = entradaTexto("Login: ");
 		String pass = entradaTexto("Senha: ");
+		
 		if(retornaLogin(user,pass) == true) {
 			System.out.println("\nUsuario Logado \n");
 			while (true) {  
 	            String opcao = menuUsuario();  
 	  
-	            if (opcao.equalsIgnoreCase("s")) {  
-	            	if(updateLogin(1) == true) {
+	            if (opcao.equalsIgnoreCase("s"))  
+	            	if(updateLogin(1) == true)
 	            		System.out.println("\nSenha alterada com sucesso \n");
-	            	}
-	            } else if (opcao.equalsIgnoreCase("n")) {
-	            	if(updateLogin(2) == true) {
+	            
+	            if (opcao.equalsIgnoreCase("n"))
+	            	if(updateLogin(2) == true)
 	            		System.out.println("\nNome atualizado com sucesso \n");
-	            	} 
-	            } else if (opcao.equalsIgnoreCase("e")) {
-	            	if(updateLogin(3) == true) {
+	            
+	            if (opcao.equalsIgnoreCase("e"))
+	            	if(updateLogin(3) == true)
 	            		System.out.println("\nEndereço atualizado com sucesso \n");
-	            	}
-	            } else if (opcao.equalsIgnoreCase("t")) {
-	            	if(updateLogin(4) == true) {
+	            
+	            if (opcao.equalsIgnoreCase("t"))
+	            	if(updateLogin(4) == true)
 	            		System.out.println("\nTelefone alterada com sucesso \n");
-	            	}
-	            } else if (opcao.equalsIgnoreCase("a")) {
-	            	System.out.println("\nImplementando \n");
-	            } else if (opcao.equalsIgnoreCase("c")) {
-	            	System.out.println("\nImplementando \n");
-	            } else if (opcao.equalsIgnoreCase("l")) {
-	            	System.out.println("\nImplementando \n");
-	            } else if (opcao.equalsIgnoreCase("x")) {
+	            
+	            if (opcao.equalsIgnoreCase("a"))
+	            	addConvite();
+	            
+	            if (opcao.equalsIgnoreCase("c"))
+	            	listaConvites();
+	            
+	            if (opcao.equalsIgnoreCase("l"))
+	            	listaAmigos();
+	            
+	            if (opcao.equalsIgnoreCase("x"))
 	            	break;
-	            } else {  
-	                System.out.println("\nOpção Inválida \n");  
-	            }  
 	        }  
 
-		} else {
-			System.out.println("\nUsuario ou senha estão incorreta(s) ou usuario não cadastrado \n");
 		}
+		else
+			System.out.println("\nUsuario ou senha estão incorreta(s) ou usuario não cadastrado \n");
 	}
 	
 	private static boolean updateLogin(int selecao) {
@@ -140,44 +145,48 @@ public class Main {
 			if(login.equals(u.getLogin())) {
 				switch(selecao) {
 				case 1:
-					senha = entradaTexto("Digite nova senha: ");
+					senha = entradaTexto("\nDigite nova senha: ");
 					if(senha.equals(u.getSenha())) {
 						System.out.println("\nSenha não atualizada, igual a anterior \n");
 						return false;
-					} else {
+					}
+					else {
 						u.setSenha(senha);
 						contas.set(i, u);
 						break;
 					}
 				case 2:
 					System.out.println("\nNome cadastrado: " + nome);
-					nome = entradaTexto("Digite a atualização do seu nome: ");
+					nome = entradaTexto("\nDigite a atualização do seu nome: ");
 					if(nome.equals(u.getNome())) {
 						System.out.println("\nNome não atualizada, igual a anterior \n");
 						return false;
-					} else {
+					}
+					else {
 						u.setNome(nome);
 						contas.set(i, u);
 						break;
 					}
 				case 3:
 					System.out.println("\nEndereço cadastrado: " + endereco);
-					endereco = entradaTexto("Digite novo endereço: ");
+					endereco = entradaTexto("\nDigite novo endereço: ");
 					if(endereco.equals(u.getEndereco())) {
 						System.out.println("\nEndereço não atualizada, igual a anterior \n");
 						return false;
-					} else {
+					}
+					else {
 						u.setEndereco(endereco);
 						contas.set(i, u);
 						break;
 					}
 				case 4:
 					System.out.println("\nTelefone cadastrado: " + telefone);
-					telefone = entradaNumero("Digite novo telefone: ");
+					telefone = entradaNumero("\nDigite novo telefone: ");
 					if(telefone == (u.getTelefone())) {
 						System.out.println("\nTelefone não atualizada, igual a anterior \n");
 						return false;
-					} else {
+					}
+					else {
 						u.setTelefone(telefone);
 						contas.set(i, u);
 						break;
@@ -192,21 +201,160 @@ public class Main {
         }
 		return false;
 	}
+		
+	private static void listaAmigos() {
+		if (amigos.size() == 0)
+            System.out.println("\nNenhum amigo \n");
+		else {
+        	String lamigos = "";
+        	
+            for (int i = 0; i < amigos.size(); i++) {
+            	Amigo a = amigos.get(i);
+            	if(a.getConta().equals(Main.login) && a.getStatus() == true)
+            		lamigos = "\n* " + a.getAmigo();
+            }
+            
+            for (int i = 0; i < amigos.size(); i++) {
+            	Amigo a = amigos.get(i);
+            	if(a.getAmigo().equals(Main.login) && a.getStatus() == true)
+            		lamigos = "\n* " + a.getConta();
+            }
+            
+            if(lamigos == "")
+            	System.out.println("\nNenhum amigo \n");
+            else
+            	System.out.println("\nLista de amigos: " + lamigos + "\n");
+        }
+	}
+	
+	private static void listaConvites() {
+		if (amigos.size() == 0)
+            System.out.println("\nNenhum convite \n");
+		else {
+        	String lamigos = "";
+            
+        	for (int i = 0; i < amigos.size(); i++) {
+            	Amigo a = amigos.get(i);
+            	if(a.getAmigo().equals(Main.login) && a.getStatus() == false)
+            		lamigos = "\n* " + a.getConta();
+            }
+            
+            if(lamigos == "")
+            	System.out.println("\nNenhum convite \n");
+            else {
+            	System.out.println("\nLista de convites: " + lamigos + "\n");
+            	
+            	String aceitar = entradaTexto("\nDigite o login do usuario, que será aceito o convite de amigo:");
+            	if(aceitarConvite(aceitar) == true)
+            		System.out.println("\nConvite aceito\n");
+            	else
+            		System.out.println("\nUsuario não encontrado\n");
+            }
+        }
+	}
+	
+	private static void addConvite() {
+		while(true) {
+			String statusc, lamigo = entradaTexto("\nDigite o login do usuario, que será enviado o convite:");
+			
+			if(retornaAmigo(lamigo) == true) {
+				enviarConvite(lamigo);
+				break;
+			}
+			else {
+				statusc = entradaTexto("\nConvite não enviado, deseja continuar? S/N : ");
+				if(statusc.equalsIgnoreCase("n"))
+					break;
+			}
+			
+		}
+	}
+			
+	private static boolean aceitarConvite(String lamigo) {
+		if(retornaConvite(lamigo) == true) {
+			for (int i = 0; i < amigos.size(); i++) {
+            	Amigo a = amigos.get(i);
+            	if(a.getAmigo().equals(Main.login) && a.getStatus() == false && a.getConta().equals(lamigo)) {
+            		a.setStatus(true);
+                	amigos.set(i, a);
+                	return true;
+            	}
+            }
+			
+		}
+		return false;
+	}
+	
+	private static boolean retornaAmigo(String lamigo) {
+		if(lamigo.equals(Main.login)) {
+			System.out.println("\nMesmo nome do seu usuario \n");
+			return false;
+		}
+		
+		for (int i = 0; i < amigos.size(); i++) {
+        	Amigo a = amigos.get(i);
+        	if(a.getConta().equals(Main.login) || a.getAmigo().equals(Main.login))
+        		if(a.getAmigo().equals(lamigo) || a.getConta().equals(lamigo)) {
+        			if(a.getStatus() == true) {
+        				System.out.println("\nJá são Amigos \n");
+        				return false;
+        			}
+        			else {
+        				System.out.println("\nJa existe um Convite \n");
+        				return false;
+        			}
+        		}
+        }
+		
+		for (int i = 0; i < contas.size(); i++) {
+        	Conta u = contas.get(i);
+        	if(lamigo.equals(u.getLogin()))
+        		return true;
+        }
+		
+		return false;
+	}
+	
+	private static boolean retornaConvite(String lamigo) {
+		if(lamigo.equals(Main.login)) {
+			System.out.println("\nMesmo nome do seu usuario \n");
+			return false;
+		}
+		
+		for (int i = 0; i < amigos.size(); i++) {
+        	Amigo a = amigos.get(i);
+        	if(a.getConta().equals(login))
+        		if(a.getAmigo().equals(lamigo)) {
+        			if(a.getStatus() == true) {
+        				System.out.println("\nJá são Amigos \n");
+        				return false;
+        			}
+        		}
+        }
+		
+		for (int i = 0; i < contas.size(); i++) {
+        	Conta u = contas.get(i);
+        	if(lamigo.equals(u.getLogin()))
+        		return true;
+        }
+		
+		return false;
+	}	
 	
 	private static boolean retornaLogin(String user, String pass) {
-		if (contas.size() == 0) {  
+		if (contas.size() == 0)
             System.out.println("\nNão existem cadastros !!! \n");
-        } else {  
+		else {  
             for (int i = 0; i < contas.size(); i++) {
             	Conta u = contas.get(i);
             	if(user.equals(u.getLogin()) && (pass.equals(u.getSenha()))) {
-            		if(login != u.getLogin()) {
-            			login = u.getLogin();
-                		senha = u.getSenha();
-                		nome = u.getNome();
-                		cpf = u.getCpf();
-                		endereco = u.getEndereco();
-                		telefone = u.getTelefone();
+            		if(Main.login != u.getLogin()) {
+            			Main.login = u.getLogin();
+                		Main.senha = u.getSenha();
+                		Main.nome = u.getNome();
+                		Main.cpf = u.getCpf();
+                		Main.endereco = u.getEndereco();
+                		Main.telefone = u.getTelefone();
             		}
             		return true;
             	}
@@ -216,35 +364,44 @@ public class Main {
 	}
 	
 	private static boolean retornaLogin(String login) {
-		if (contas.size() == 0) {  
+		if (contas.size() == 0)  
 			return false;
-        } else {  
+		else {  
             for (int i = 0; i < contas.size(); i++) {
             	Conta u = contas.get(i);
-            	if(login.equals(u.getLogin())) {
+            	if(login.equals(u.getLogin()))
             		return true;
-            	}
             }
-        }
+        }	
 		return false;        
 	}
 	
 	private static boolean retornaLogin(Long cpf) {
-		if (contas.size() == 0) {  
+		if (contas.size() == 0)  
 			return false;
-        } else {  
+		else {  
             for (int i = 0; i < contas.size(); i++) {
             	Conta u = contas.get(i);
-            	if(cpf.equals(u.getCpf())) {
+            	if(cpf.equals(u.getCpf()))
             		return true;
-            	}
             }
         }
 		return false;        
 	}
 	
+	private static void enviarConvite(String lamigo) {  
+        Amigo a = new Amigo();  
+        
+        a.setConta(login);  
+        a.setAmigo(lamigo);
+        a.setStatus(false);
+        
+        amigos.add(a);
+        System.out.println("\nConvite enviado. \n");
+	}
+	
 	private static String menuCadastro() {  
-        System.out.println("Selecione a opção:");  
+        System.out.println("\nSelecione a opção:");  
         System.out.println("C - Criar usuario");  
         System.out.println("L - Login no sistema");  
         System.out.println("X - Sair");  
@@ -252,7 +409,7 @@ public class Main {
     }
 	
 	private static String menuUsuario() {  
-        System.out.println("Selecione a opção:");
+        System.out.println("\nSelecione a opção:");
         System.out.println("-------------------------");
         System.out.println("A - Adicionar amigos");
         System.out.println("C - Aceitar convites");
